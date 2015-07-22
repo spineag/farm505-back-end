a56a23ecad7dbfe153b807cc5410b8cafc163ca2
spineag@gmail.com <quadro57>
2015-07-22 11:44:19 +0300

    addUserXP

---
+++ api-v1-0/addUserXP.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $result = $mainDb->query("SELECT xp FROM users WHERE id =".$_POST['userId']);
+        if ($result) {
+            $arr = $result->fetch();
+            $count = $arr['xp'];
+            $count = (int)$count + (int)$_POST['count'];
+            $result = $mainDb->update(
+                'users',
+                ['xp' => $count],
+                ['id' => $_POST['userId']],
+                ['int'],
+                ['int']);
+        } else {
+            $json_data['id'] = 2;
+            throw new Exception("Bad request to DB!");
+        }
+
+        $json_data['message'] = '';
+        echo json_encode($json_data);
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 'error';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 'error';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
