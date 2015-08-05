0fc7c1daf9ad47bf34a6a5d2fb6d9fe850aa9cb6
spineag <spineag@gmail.com>
2015-08-05 20:00:21 +0300

    craftRecipeOnFabrica

---
+++ api-v1-0/craftRecipeOnFabrica.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $result = $mainDb->delete('user_recipe_fabrica',
+            ['user_id' => $_POST['userId'], 'id' => $_POST['dbId']],
+            ['int', 'int']);
+
+        if ($result) {
+            $json_data['message'] = '';
+            echo json_encode($json_data);
+        } else {
+            $json_data['id'] = 2;
+            throw new Exception("Bad request to DB!");
+        }
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 'error';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 'error';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
