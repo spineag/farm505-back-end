abc974e0320337e959105348c2abd15b263c944e
spineag <spineag@gmail.com>
2015-08-06 20:23:03 +0300

    getUserAnimal

---
+++ api-v1-0/getUserAnimal.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $resp = [];
+        $result = $mainDb->query("SELECT * FROM user_animal WHERE user_id =".$_POST['userId']);
+        if ($result) {
+            $arr = $result->fetchAll();
+            foreach ($arr as $value => $dict) {
+                $res = [];
+                $res['id'] = $dict['id'];
+                $res['animal_id'] = $dict['animal_id'];
+                $res['user_db_building_id'] = $dict['user_db_building_id'];
+                $res['time_work'] = time() - $dict['raw_time_start'];
+                $resp[] = $res;
+            }
+        } else {
+            $json_data['id'] = 2;
+            throw new Exception("Bad request to DB!");
+        }
+
+        $json_data['message'] = $resp;
+        echo json_encode($json_data);
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 'error';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 'error';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
