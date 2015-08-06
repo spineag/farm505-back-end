9aee3ea460177f0e2ed33f24f31e2053de8b736b
spineag <spineag@gmail.com>
2015-08-06 14:29:03 +0300

    addUserTRee

---
+++ api-v1-0/addUserTree.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $time = time();
+        $result = $mainDb->insert('user_tree',
+            ['user_id' => $_POST['userId'], 'user_db_building_id' => $_POST['dbId'], 'state' => 1, 'time_start' => $time],
+            ['int', 'int', 'int', 'int']);
+
+        $result = $mainDb->query("SELECT * FROM user_tree WHERE user_id =".$_POST['userId']." AND time_start=".$time);
+
+        if ($result) {
+            $arr= $result->fetch();
+            $json_data['message'] = $arr['id'];
+            echo json_encode($json_data);
+        } else {
+            $json_data['id'] = 2;
+            $json_data['status'] = 'error';
+            $json_data['message'] = 'bad query';
+        }
+
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 'error';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 'error';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
