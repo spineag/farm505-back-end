3c191192abf97c7eee4e1b21d4a14231461ab86b
spineag@gmail.com <quadro57>
2015-09-01 17:52:36 +0300

    getPaperItems

---
+++ api-v1-0/getPaperItems.php
@@ -0,0 +1,51 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $resp = [];
+        $result = $mainDb->query("SELECT * FROM user_market_item WHERE in_papper = 1 ORDER BY RAND() LIMIT 9");
+        if ($result) {
+            $arr = $result->fetchAll();
+            foreach ($arr as $value => $dict) {
+                $res = [];
+                $res['id'] = $dict['id'];
+                $res['user_id'] = $dict['user_id'];
+                $res['cost'] = $dict['cost'];
+                $res['resource_id'] = time() - $dict['resource_id'];
+                $res['resource_count'] = time() - $dict['resource_count'];
+
+                $result2 = $mainDb->query("SELECT * FROM users WHERE id =".$dict['user_id']);
+                $arr = $result2->fetch();
+                $res['user_social_id'] = $arr['social_id'];
+
+                $resp[] = $res;
+            }
+        } else {
+            $json_data['id'] = 2;
+            throw new Exception("Bad request to DB!");
+        }
+
+        $json_data['message'] = $resp;
+        echo json_encode($json_data);
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 'error';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 'error';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
