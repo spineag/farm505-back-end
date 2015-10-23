514fffdb52330022e60024223897ae5219c99d47
spineag@gmail.com <quadro57>
2015-10-23 16:31:59 +0300

    wild

---
+++ api-v1-0/ME_moveWild.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $result = $mainDb->update(
+            'data_map_wild',
+            ['pos_x' => $_POST['posX'], 'pos_y' => $_POST['posY']],
+            ['id' => $_POST['dbId']],
+            ['int', 'int'],
+            ['int']);
+
+        if ($result) {
+            $json_data['message'] = '';
+        } else {
+            $json_data['id'] = 2;
+            $json_data['status'] = 'error';
+            $json_data['message'] = 'bad query';
+        }
+
+        echo json_encode($json_data);
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 'error';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 'error';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
