ca8ef505d45cdabafd6e546d2a6b03955fa02c6b
spineag@gmail.com <quadro57>
2015-11-16 17:07:16 +0200

    orders

---
+++ api-v1-0/addUserOrder.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $result = $mainDb->insert('user_order',
+            ['user_id' => $_POST['userId'], 'ids' => $_POST['ids'], 'counts' => $_POST['counts'], 'xp' => $_POST['xp'], 'coins' => $_POST['coins'], 'add_coupone' => $_POST['addCoupone']],
+            ['int', 'str', 'str', 'int', 'int', 'int']);
+
+        $result = $mainDb->query("SELECT * FROM user_order WHERE user_id =".$_POST['userId']." AND ids=".$_POST['ids']." AND counts=".$_POST['counts']);
+
+        if ($result) {
+            $arr= $result->fetch();
+            $json_data['message'] = $arr['id'];
+            echo json_encode($json_data);
+        } else {
+            $json_data['id'] = 2;
+            $json_data['status'] = 'error';
+            $json_data['message'] = 'bad query';
+        }
+
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 'error';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 'error';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
---
+++ api-v1-0/getUserOrder.php
@@ -0,0 +1,47 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/public/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $resp = [];
+        $result = $mainDb->query("SELECT * FROM user_order WHERE user_id =".$_POST['userId']);
+        if ($result) {
+            $arr = $result->fetchAll();
+            foreach ($arr as $value => $dict) {
+                $res = [];
+                $res['id'] = $dict['id'];
+                $res['ids'] = $dict['ids'];
+                $res['counts'] = $dict['counts'];
+                $res['xp'] = $dict['xp'];
+                $res['coins'] = $dict['coins'];
+                $res['add_coupone'] = $dict['add_coupone'];
+                $resp[] = $res;
+            }
+        } else {
+            $json_data['id'] = 2;
+            throw new Exception("Bad request to DB!");
+        }
+
+        $json_data['message'] = $resp;
+        echo json_encode($json_data);
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 'error';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 'error';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
