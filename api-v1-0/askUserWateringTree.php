25ee051e596bc3a368007a012f623d0dd4477c10
spineag@gmail.com <quadro57>
2015-12-14 17:07:08 +0200

    adds for user tree

--- api-v1-0/askUserWateringTree.php
+++ api-v1-0/askUserWateringTree.php
@@ -9,26 +9,19 @@ if (isset($_POST['userId']) && !empty($_POST['userId'])) {
     $channelId = 1; // VK
 
     try {
-        $resp = [];
-        $result = $mainDb->query("SELECT * FROM user_tree WHERE user_id =".$_POST['userId']);
-        if ($result) {
-            $arr = $result->fetchAll();
-            foreach ($arr as $value => $dict) {
-                $res = [];
-                $res['id'] = $dict['id'];
-                $res['state'] = $dict['state'];
-                $res['user_db_building_id'] = $dict['user_db_building_id'];
-                $res['time_work'] = time() - $dict['time_start'];
-                $res['time_start'] = $dict['time_start'];
-                $res['fixed_user_id'] = $dict['fixed_user_id'];
-                $resp[] = $res;
-            }
-        } else {
+        $result = $mainDb->update(
+            'user_tree',
+            ['state' => $_POST['state'], 'fixed_user_id' => '-1', 'time_start' => time()],
+            ['id' => $_POST['id']],
+            ['int', 'int', 'int'],
+            ['int']);
+
+        if (!$result) {
             $json_data['id'] = 2;
             throw new Exception("Bad request to DB!");
         }
 
-        $json_data['message'] = $resp;
+        $json_data['message'] = '';
         echo json_encode($json_data);
     }
     catch (Exception $e)
