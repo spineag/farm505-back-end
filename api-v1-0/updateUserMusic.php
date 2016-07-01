1bbb019da83530f85dfa55fede395fb50dcd9a3a
spineag@gmail.com <quadro57>
2016-07-01 12:01:43 +0300

    update user music and sound

---
+++ api-v1-0/updateUserMusic.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/php/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/php/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $mainDb = $app->getMainDb();
+    $channelId = 1; // VK
+
+    try {
+        $result = $mainDb->query('UPDATE users SET music ='.$_POST['music'].' WHERE id='.$_POST['userId']);
+        if (!$result) {
+            $json_data['id'] = 2;
+            throw new Exception("Bad request to DB!");
+        }
+
+        $json_data['message'] = '';
+        echo json_encode($json_data);
+    }
+    catch (Exception $e)
+    {
+        $json_data['status'] = 's207';
+        $json_data['message'] = $e->getMessage();
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 's181';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
