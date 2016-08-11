5ce87c41a5d592054ae73cd454da6ac7541b45be
spineag <spineag@gmail.com>
2016-08-11 16:25:17 +0300

    add for addUserResource

--- api-v1-0/addUserResource.php
+++ api-v1-0/addUserResource.php
@@ -7,93 +7,46 @@ if (isset($_POST['userId']) && !empty($_POST['userId'])) {
     $app = Application::getInstance();
     $mainDb = $app->getMainDb();
     $channelId = 1; // VK
-    if (isset($_POST['countAll'])) {
+    if ($app->checkSessionKey($_POST['userId'], $_POST['sessionKey'])) {
+        $m = md5($_POST['userId'] . $_POST['resourceId'] . $_POST['countAll'] . $app->md5Secret());
+        if ($m != $_POST['hash']) {
+            $json_data['id'] = 6;
+            $json_data['status'] = 's360';
+            $json_data['message'] = 'wrong hash';
+            echo json_encode($json_data);
+        } else {
+            try {
+                $result = $mainDb->query("SELECT * FROM user_resource WHERE user_id =" . $_POST['userId'] . " AND resource_id=" . $_POST['resourceId']);
+                if ($result) {
+                    $result = $mainDb->query('UPDATE user_resource SET count = ' . $_POST['countAll'] . ' WHERE user_id=' . $_POST['userId'] . ' AND resource_id = ' . $_POST['resourceId']);
+                    $text = 'update';
+                } else {
+                    $result = $mainDb->query('INSERT INTO user_resource SET user_id=' . $_POST['userId'] . ', resource_id=' . $_POST['resourceId'] . ', count=' . $_POST['countAll']);
+                    $text = 'insert';
+                }
 
-        if ($app->checkSessionKey($_POST['userId'], $_POST['sessionKey'])) {
-            $m = md5($_POST['userId'] . $_POST['resourceId'] . $_POST['countAll'] . $app->md5Secret());
-            if ($m != $_POST['hash']) {
-                $json_data['id'] = 6;
-                $json_data['status'] = 's360';
-                $json_data['message'] = 'wrong hash';
-                echo json_encode($json_data);
-            } else {
-                try {
-                    $result = $mainDb->query("SELECT count FROM user_resource WHERE user_id =" . $_POST['userId'] . " AND resource_id=" . $_POST['resourceId']);
-                    $arr = $result->fetch();
-                    if (count($arr) > 0) {
-                        $result = $mainDb->query('UPDATE user_resource SET count = ' . $_POST['countAll'] . ' WHERE user_id=' . $_POST['userId'] . ' AND resource_id = ' . $_POST['resourceId']);
-                        $text = 'update';
-                    } else {
-                        $result = $mainDb->query('INSERT INTO user_resource SET user_id=' . $_POST['userId'] . ', resource_id=' . $_POST['resourceId'] . ', count=' . $_POST['countAll']);
-                        $text = 'insert';
-                    }
-
-                    if ($result) {
-                        $json_data['message'] = '';
-                        echo json_encode($json_data);
-                    } else {
-                        $json_data['id'] = 2;
-                        $json_data['status'] = 's024';
-                        $json_data['message'] = 'bad query:: ' . $text;
-                        echo json_encode($json_data);
-                    }
-                } catch (Exception $e) {
-                    $json_data['status'] = 's025';
-                    $json_data['message'] = $e->getMessage();
+                if ($result) {
+                    $json_data['message'] = '';
+                    echo json_encode($json_data);
+                } else {
+                    $json_data['id'] = 2;
+                    $json_data['status'] = 's024';
+                    $json_data['message'] = 'bad query:: ' . $text;
                     echo json_encode($json_data);
                 }
-            }
-        } else {
-                $json_data['id'] = 13;
-                $json_data['status'] = 's221';
-                $json_data['message'] = 'bad sessionKey';
-                echo json_encode($json_data);
-            }
-        } else {
-        if ($app->checkSessionKey($_POST['userId'], $_POST['sessionKey'])) {
-            $m = md5($_POST['userId'] . $_POST['resourceId'] . $_POST['count'] . $app->md5Secret());
-            if ($m != $_POST['hash']) {
-                $json_data['id'] = 6;
-                $json_data['status'] = 's360a';
-                $json_data['message'] = 'wrong hash';
-                echo json_encode($json_data);
-            } else {
-                try {
-                    $result = $mainDb->query("SELECT count FROM user_resource WHERE user_id =" . $_POST['userId'] . " AND resource_id=" . $_POST['resourceId']);
-                    $arr = $result->fetch();
-                    if (count($arr) > 0) {
-                        $count = $arr['count'];
-                        $count = (int)$count + (int)$_POST['count'];
-                        $result = $mainDb->query('UPDATE user_resource SET count = ' . $count . ' WHERE user_id=' . $_POST['userId'] . ' AND resource_id = ' . $_POST['resourceId']);
-                        $text = 'update';
-                    } else {
-                        $result = $mainDb->query('INSERT INTO user_resource SET user_id=' . $_POST['userId'] . ', resource_id=' . $_POST['resourceId'] . ', count=' . $_POST['count']);
-                        $text = 'insert';
-                    }
-
-                    if ($result) {
-                        $json_data['message'] = '';
-                        echo json_encode($json_data);
-                    } else {
-                        $json_data['id'] = 2;
-                        $json_data['status'] = 's024';
-                        $json_data['message'] = 'bad query:: ' . $text;
-                        echo json_encode($json_data);
-                    }
-                } catch (Exception $e) {
-                    $json_data['status'] = 's025';
-                    $json_data['message'] = $e->getMessage();
-                    echo json_encode($json_data);
-                }
-            }
-        } else {
-                $json_data['id'] = 13;
-                $json_data['status'] = 's221';
-                $json_data['message'] = 'bad sessionKey';
+            } catch (Exception $e) {
+                $json_data['status'] = 's025';
+                $json_data['message'] = $e->getMessage();
                 echo json_encode($json_data);
             }
         }
     } else {
+        $json_data['id'] = 13;
+        $json_data['status'] = 's221';
+        $json_data['message'] = 'bad sessionKey';
+        echo json_encode($json_data);
+    }
+} else {
     $json_data['id'] = 1;
     $json_data['status'] = 's026';
     $json_data['message'] = 'bad POST[userId]';
