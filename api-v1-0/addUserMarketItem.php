2dd21ca1acab5765407806c1014c805db1d2a4d1
spineag <spineag@gmail.com>
2016-09-16 13:51:15 +0300

    New changelist

--- api-v1-0/addUserMarketItem.php
+++ api-v1-0/addUserMarketItem.php
@@ -18,7 +18,7 @@ if (isset($_POST['userId']) && !empty($_POST['userId'])) {
         } else {
             try {
                 $time = time();
-                $result = $mainDb->queryWithAnswerId('INSERT INTO user_market_item SET user_id=' . $_POST['userId'] . ', buyer_id=0, resource_id=' . $_POST['resourceId'] . ', time_start=' . $time . ', time_sold=0, cost=' . $_POST['cost'] . ', resource_count=' . $_POST['count'] . ', in_papper=' . $_POST['inPapper'] . ', number_cell=' . $_POST['numberCell'] . ', time_in_papper=' . $_POST['timeInPapper']);
+                $result = $mainDb->queryWithAnswerId('INSERT INTO user_market_item SET user_id='.$_POST['userId'].', buyer_id=0, resource_id='.$_POST['resourceId'].', time_start='.$time .',time_sold=0, cost='.$_POST['cost'].', resource_count='.$_POST['count'].', in_papper='.$_POST['inPapper'].', number_cell='.$_POST['numberCell'].', time_in_papper='.$_POST['timeInPapper']);
                 if ($result) {
                     $res = [];
                     $res['id'] = $result[1];
