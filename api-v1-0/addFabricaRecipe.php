c9859b71911b79f7c04168b54423caf5b737a5d5
Volodymyr Ivchyk <volodymyr.ivchyk@joyrocks.com>
2016-10-26 22:16:39 +0300

    Use queryLastId

--- api-v1-0/addFabricaRecipe.php
+++ api-v1-0/addFabricaRecipe.php
@@ -19,9 +19,12 @@ if (isset($_POST['userId']) && !empty($_POST['userId'])) {
         } else {
             try {
                 $time = time();
-                $result = $shardDb->queryWithAnswerId('INSERT INTO user_recipe_fabrica SET user_id=' . $userId . ', user_db_building_id=' . $_POST['dbId'] . ', recipe_id=' . $_POST['recipeId'] . ', delay_time=' . $_POST['delay'] . ', time_start=' . $time);
+                $result = $shardDb->queryLastId('INSERT INTO user_recipe_fabrica 
+                                                 SET user_id=' . $userId . ', user_db_building_id=' .
+                                                 $_POST['dbId'] . ', recipe_id=' . $_POST['recipeId'] . ', delay_time=' .
+                                                 $_POST['delay'] . ', time_start=' . $time);
                 if ($result) {
-                    $json_data['message'] = $result[1];
+                    $json_data['message'] = $result;
                     echo json_encode($json_data);
                 } else {
                     $json_data['id'] = 2;
