6d78833efea37e136ae641cd75b0a3d986c21df0
Volodymyr Ivchyk <volodymyr.ivchyk@joyrocks.com>
2016-10-26 22:53:46 +0300

    Revamped getUserMarketItem.php

--- api-v1-0/getUserMarketItem.php
+++ api-v1-0/getUserMarketItem.php
@@ -3,6 +3,7 @@
 include_once($_SERVER['DOCUMENT_ROOT'] . '/php/api-v1-0/library/Application.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/php/api-v1-0/library/defaultResponseJSON.php');
 
+const TIME_GAP = 5 * 60 * 60;
 if (isset($_POST['userSocialId']) && !empty($_POST['userSocialId'])) {
     $app = Application::getInstance();
     $channelId = 1; // VK
@@ -13,70 +14,43 @@ if (isset($_POST['userSocialId']) && !empty($_POST['userSocialId'])) {
         $shardDb = $app->getShardDb($userId);
         try {
             $resp = [];
-            $result = $mainDb->query("SELECT * FROM users WHERE social_id =".$_POST['userSocialId']);
+            $result = $mainDb->query("SELECT market_cell, id FROM users WHERE social_id =".$_POST['userSocialId']);
             $arr = $result->fetch();
-            $responce['market_cell'] = $arr['market_cell'];
+            $response['market_cell'] = $arr['market_cell'];
             $id = $arr['id'];
+            $time = time() - TIME_GAP;
+            $shardDb->query("UPDATE user_market_item SET in_papper=0 AND time_in_papper = 0 WHERE user_id = ". $id . " 
+            AND in_papper = 1 AND time_in_papper < " . $time);
 
-            $result = $shardDb->query("SELECT * FROM user_market_item WHERE user_id =".$id);
-            if ($result) {
-                $arr = $result->fetchAll();
-                foreach ($arr as $value => $dict) {
-                    $res = [];
-                    $res['id'] = $dict['id'];
-                    $res['buyer_id'] = $dict['buyer_id'];
-                    $res['time_start'] = $dict['time_start'];
-                    $res['time_sold'] = $dict['time_sold'];
-                    $res['cost'] = $dict['cost'];
-                    $res['resource_id'] = $dict['resource_id'];
-                    $res['resource_count'] = $dict['resource_count'];
-                    $res['number_cell'] = $dict['number_cell'];
-                    $res['time_in_papper'] = $dict['time_in_papper'];
-                    $res['level'] = $dict['level'];
-                    if ((int)$dict['in_papper'] == 1) {
-                        if (time() > (int)$dict['time_in_papper'] + 5*60*60) {
-                            $res['in_papper'] = 0;
-                            $res['time_in_papper'] = 0;
-                            $resultUpdate = $shardDb->query('UPDATE user_market_item SET in_papper=0 AND time_in_papper = 0 WHERE id='.$res['id']);
-                        } else {
-                            $res['in_papper'] = 1;
-                        }
-                    } else {
-                        $res['in_papper'] = 0;
-                    }
-
-                    if ($dict['buyer_id'] > 0) {
-                        $result2 = $mainDb->query("SELECT * FROM users WHERE id =".$dict['buyer_id']);
+            $result = $shardDb->query("SELECT * FROM user_market_item WHERE user_id =" . $id);
+            while ($res =  $result->fetch()) {
+                if ($res['buyer_id'] > 0) {
+                    $result2 = $mainDb->query("SELECT * FROM users WHERE id =".$res['buyer_id']);
+                    $arr = $result2->fetch();
+                    $res['buyer_social_id'] = $arr['social_id'];
+                } else {
+                    if ($id == $_POST['userId']) {
+                        $result2 = $mainDb->query("SELECT * FROM users WHERE social_id = 1");
                         $arr = $result2->fetch();
-                        $res['buyer_social_id'] = $arr['social_id'];
-                    } else {
-                        if ($id == $_POST['userId']) {
-                            $result2 = $mainDb->query("SELECT * FROM users WHERE social_id = 1");
-                            $arr = $result2->fetch();
-                            if (time() - $dict['time_start'] > 24*60*60) {
-                                $result = $shardDb->update(
-                                    'user_market_item',
-                                    ['buyer_id' => $arr['id'], 'time_sold' => time(), 'in_papper' => 0],
-                                    ['id' => $dict['id']],
-                                    ['int', 'int','int'],
-                                    ['int']);
-                                $res['buyer_social_id'] = 1;
-                                $res['buyer_id'] = $arr['id'];
-                                $res['time_sold'] = time();
-                                $res['in_papper'] = 0;
-                            }
+                        if (time() - $dict['time_start'] > 24*60*60) {
+                            $result = $shardDb->update(
+                                'user_market_item',
+                                ['buyer_id' => $arr['id'], 'time_sold' => time(), 'in_papper' => 0],
+                                ['id' => $dict['id']],
+                                ['int', 'int','int'],
+                                ['int']);
+                            $res['buyer_social_id'] = 1;
+                            $res['buyer_id'] = $arr['id'];
+                            $res['time_sold'] = time();
+                            $res['in_papper'] = 0;
                         }
                     }
-                    $resp[] = $res;
                 }
-            } else {
-                $json_data['id'] = 2;
-                $json_data['status'] = 's303';
-                throw new Exception("Bad request to DB!");
+                $resp[] = $res;
             }
 
             $responce['items'] = $resp;
-            $json_data['message'] = $responce;
+            $json_data['message'] = $response;
             echo json_encode($json_data);
         }
         catch (Exception $e)
