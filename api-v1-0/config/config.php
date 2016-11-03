1da7bfb98aaf2006d59b1da2901805015263eff9
spineag <spineag@gmail.com>
2016-11-03 11:20:09 +0200

    New changelist

--- api-v1-0/config/config.php
+++ api-v1-0/config/config.php
@@ -13,7 +13,7 @@ define("PASSWORD","Zuthoopo3S");
 define("DBPORT", "3306");
 
 define("SERVER_OK", '148.251.121.199');
-define("DB_OK", 'ok');
+define("DB_OK", 'farm_ok');
 define("SERVER_DB_OK", '192.168.1.23');
 define("USER_OK", 'ninja505');
 define("PASSWORD_OK","Zuthoopo3S");
