5f7698c31071b764a242bb4f802fd4567d456597
spineag@gmail.com <quadro57>
2016-11-16 10:09:41 +0200

    memcache with channelId

--- api-v1-0/getDataLockedLand.php
+++ api-v1-0/getDataLockedLand.php
@@ -17,13 +17,13 @@ $mainDb = $app->getMainDb($channelId);
 $memcache = $app->getMemcache();
 
 try {
-//    $resp = $memcache->get('getDataLockedLand');
-//    if (!$resp) {
-        $result = $mainDb->query("SELECT unlocked_land FROM users WHERE id = " . $_POST['userId']);
-        $u = $result->fetchAll();
-        $u = $u[0]['unlocked_land'];
-        $arrLocked = explode("&", $u);
+    $result = $mainDb->query("SELECT unlocked_land FROM users WHERE id = " . $_POST['userId']);
+    $u = $result->fetchAll();
+    $u = $u[0]['unlocked_land'];
+    $arrLocked = explode("&", $u);
 
+    $result = $memcache->get('getDataLockedLand'.$channelId);
+    if (!$result) {
         $result = $mainDb->query("SELECT * FROM data_locked_land");
         if ($result) {
             $lands = $result->fetchAll();
@@ -32,19 +32,19 @@ try {
             $json_data['status'] = 's288';
             throw new Exception("Bad request to DB!");
         }
-        $resp = [];
-        if (!empty($lands)) {
-            foreach ($lands as $key => $land) {
-                if (in_array($land['map_building_id'], $arrLocked)) continue;
-                $resp[] = $land;
-            }
-        } else {
-            $json_data['id'] = 2;
-            $json_data['status'] = 's289';
-            throw new Exception("Bad request to DB!");
+        $memcache->set('getDataLockedLand'.$channelId, $result, MEMCACHED_DICT_TIME);
+    }
+    $resp = [];
+    if (!empty($lands)) {
+        foreach ($lands as $key => $land) {
+            if (in_array($land['map_building_id'], $arrLocked)) continue;
+            $resp[] = $land;
         }
-//        $memcache->set('getDataLockedLand', $resp, MEMCACHED_DICT_TIME);
-//    }
+    } else {
+        $json_data['id'] = 2;
+        $json_data['status'] = 's289';
+        throw new Exception("Bad request to DB!");
+    }
 
     $json_data['message'] = $resp;
     echo json_encode($json_data);
