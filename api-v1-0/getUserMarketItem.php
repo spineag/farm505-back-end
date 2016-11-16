21497af39c8984e3ce9f9d6e9fc7bd62f3e5dc24
spineag@gmail.com <quadro57>
2016-11-16 17:58:31 +0200

    fix

--- api-v1-0/getUserMarketItem.php
+++ api-v1-0/getUserMarketItem.php
@@ -16,14 +16,15 @@ if (isset($_POST['userSocialId']) && !empty($_POST['userSocialId'])) {
         $shardDb = $app->getShardDb($userId, $channelId);
         try {
             $resp = [];
-            $result = $mainDb->query("SELECT market_cell FROM users WHERE id =".$userId);
+            $result = $mainDb->query("SELECT id, market_cell FROM users WHERE id =".$userId);
             $arr = $result->fetch();
+            $idU = $arr['id'];
             $response['market_cell'] = $arr['market_cell'];
             $time = time() - TIME_GAP;
-            $shardDb->query("UPDATE user_market_item SET in_papper=0, time_in_papper = 0 WHERE user_id = ". $userId . "
+            $shardDb->query("UPDATE user_market_item SET in_papper=0, time_in_papper = 0 WHERE user_id = ". $idU . "
             AND in_papper = 1 AND time_in_papper < " . $time);
 
-            $result = $shardDb->query("SELECT * FROM user_market_item WHERE user_id =" . $userId);
+            $result = $shardDb->query("SELECT * FROM user_market_item WHERE user_id =" . $idU);
             $res = $result->fetchAll();
             foreach ($res as $value => $d) {
                 if ((int)$d['buyer_id'] > 0) {
@@ -31,20 +32,22 @@ if (isset($_POST['userSocialId']) && !empty($_POST['userSocialId'])) {
                     $arr = $result2->fetch();
                     $d['buyer_social_id'] = $arr['social_id'];
                } else {
-                    if (time() - (int)$d['time_start'] > 24*60*60) {
-                        $result2 = $mainDb->query("SELECT id FROM users WHERE social_id = 1");
-                        $arr = $result2->fetch();
+                    if ($_POST['userId'] == $idU) {
+                        if (time() - (int)$d['time_start'] > 24 * 60 * 60) {
+                            $result2 = $mainDb->query("SELECT id FROM users WHERE social_id = 1");
+                            $arr = $result2->fetch();
 //                        $result = $shardDb->update(
 //                            'user_market_item',
 //                            ['buyer_id' => $arr['id'], 'time_sold' => time(), 'in_papper' => 0],
 //                            ['id' => $d['id']],
 //                            ['int', 'int','int'],
 //                            ['int']);
-                        $result = $shardDb->query("UPDATE user_market_item SET buyer_id=".$arr['id'].", time_sold=".time().", in_papper=0 WHERE id=".$d['id']);
-                        $d['buyer_social_id'] = 1;
-                        $d['buyer_id'] = $arr['id'];
-                        $d['time_sold'] = time();
-                        $d['in_papper'] = 0;
+                            $result = $shardDb->query("UPDATE user_market_item SET buyer_id=" . $arr['id'] . ", time_sold=" . time() . ", in_papper=0 WHERE id=" . $d['id']);
+                            $d['buyer_social_id'] = 1;
+                            $d['buyer_id'] = $arr['id'];
+                            $d['time_sold'] = time();
+                            $d['in_papper'] = 0;
+                        }
                     }
                 }
                 $resp[] = $d;
