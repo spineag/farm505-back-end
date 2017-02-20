493a1238fe01d0e24a0f989fefc3665ac566f0d0
spineag <spineag@gmail.com>
2017-02-20 10:56:44 +0200

    some fixis

--- api-v1-0/payment/payment_ok.php
+++ api-v1-0/payment/payment_ok.php
@@ -73,7 +73,7 @@ class Payment {
             "10" => 240,
             "11" => 690,
             "12" => 1490,
-            "13" => 85
+            "13" => 85,
             "14" => 85
         );
         return self::$catalog;
@@ -156,7 +156,7 @@ class Payment {
         try {
             $time = date("Y-m-d H:i:s");
             $mainDb->query('INSERT INTO transactions SET uid='. $uid .', product_code='.$product_code.', time_try="'.$time.'"');
-            $mainDb->query('INSERT INTO transaction_lost SET uid='. $uid .', product_code='.$product_code.', time_buy='.time());
+            $mainDb->query('INSERT INTO transaction_lost SET uid='. $uid .', product_code='.$product_code.', time_buy="'.$time.'"');
         } catch(Exception $e) {}
     }
 
